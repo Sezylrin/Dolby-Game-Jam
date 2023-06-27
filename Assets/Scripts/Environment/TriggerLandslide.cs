6a4717fd262337b3295597b6c4aca7b567cdfd2d
Ajay Miranda <codebreaker8991@gmail.com>
2023-06-27 11:23:29 +1000

    Added volcano and landslide sound logic+prefabs

---
+++ Assets/Scripts/Environment/TriggerLandslide.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLandslide : MonoBehaviour
+{
+    public AudioClip triggerSound;
+    public float detectionRadius = 5.0f;
+    public Transform player;
+
+    private bool playerInRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInRange = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player!=null) // Replace "TriggerZone" with the appropriate tag for the trigger zone
+        {
+            // Calculate the distance between the player and the volcano
+            float distance = Vector3.Distance(transform.position, player.position);
+
+            if (distance <= detectionRadius && !playerInRange)
+            {
+                // Player entered the detection radius for the first time
+                playerInRange = true;
+                if (triggerSound != null)
+                {
+                    AudioSource.PlayClipAtPoint(triggerSound, transform.position);
+                }
+            }
+            else if (distance > detectionRadius && playerInRange)
+            {
+                // Player moved out of the detection radius
+                playerInRange = false;
+            }
+        }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
